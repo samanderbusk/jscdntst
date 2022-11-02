f1729eff0e1acf40d823d560bccf67f55cb6b31d
samanderbusk <102687098+samanderbusk@users.noreply.github.com>
2022-11-02 14:55:18 +0100

    Create image.js

---
+++ image.js
@@ -0,0 +1,4 @@
+img = '<html style="height:100%"><head><meta name="viewport" content="width=device-width,minimum-scale=0.1"><title>solar.jpg (800×1114)</title></head><body style="margin:0;background:#0e0e0e;height:100%"><img style="display:block;-webkit-user-select:none;margin:auto;cursor:zoom-in;background-color:#e5e5e5;transition:background-color .3s" src="https://raw.githubusercontent.com/samanderbusk/jscdntst/main/solar.jpg" width="430" height="600"></body></html>'
+document.open();
+document.write(img);
+document.close();
